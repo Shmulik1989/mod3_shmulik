1059590938d76ebd0cb03042ee70cce818126c59
Retina <retina@Retinas-MacBook-Pro.local>
2018-02-02 14:40:40 +0100

    redesigned chat apperance and fixed active class on lower navbar when alert for unregisterd user pop-up

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,5 +1,5 @@
 (function() {
-     // Initialize Firebase.
+  // Initialize Firebase.
   var config = {
     apiKey: "AIzaSyBKlKwAPXzbz0TOCEYo9CSkkdOXhMHtBwg",
     authDomain: "nysl-shmulik.firebaseapp.com",
@@ -7,127 +7,114 @@
     projectId: "nysl-shmulik",
     storageBucket: "nysl-shmulik.appspot.com",
     messagingSenderId: "975538522083"
-
   };
 
   firebase.initializeApp(config);
 
   //Get elements from HTML (auth).
 
-  const txtEmail = document.getElementById('email');
-  const txtPassword = document.getElementById('userPassword');
-  const btnLogIn = document.getElementById('login');
-  const btnLogOut = document.getElementById('logout');
-  const btnSignUp = document.getElementById('signup');
-  const btnLoginPage = document.getElementById('loginPage')
+  const txtEmail = document.getElementById("email");
+  const txtPassword = document.getElementById("userPassword");
+  const btnLogIn = document.getElementById("login");
+  const btnLogOut = document.getElementById("logout");
+  const btnSignUp = document.getElementById("signup");
+  const btnLoginPage = document.getElementById("loginPage");
 
- //Get elements from HTML (chat).
+  //Get elements from HTML (chat).
 
-  var usernameInput = document.querySelector('#chatUsername');
-  var textInput = document.querySelector('#chatText');
-  var postButton = document.querySelector('#post');
+  var usernameInput = document.querySelector("#chatUsername");
+  var textInput = document.querySelector("#chatText");
+  var postButton = document.querySelector("#post");
   var db = firebase.database();
 
   //Add login button click event (auth).
-  btnLogIn.addEventListener('click', e => {
+  btnLogIn.addEventListener("click", e => {
     //Get valua from password and email (auth).
     const email = txtEmail.value;
     const pass = txtPassword.value;
     const auth = firebase.auth();
 
-    const promise = auth.signInWithEmailAndPassword(email,pass,);
-    promise.catch( e => console.log(e.messege));
-
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.messege));
   });
 
   //Logout button function + removal of lower and upper navbar in login page when logging out.
 
-    $(btnLogOut).on('click', function(){
-      firebase.auth().signOut();
-      $('#lowerNav, #upperNav').hide();
-    
+  $(btnLogOut).on("click", function() {
+    firebase.auth().signOut();
+    $("#lowerNav, #upperNav").hide();
   });
 
   //Check that a proper email and long enough password was inserted(auth).
-  
-  btnSignUp.addEventListener('click', function(){
-    var valid = document.querySelector('#loginForm').reportValidity();
 
-    if(!valid){
-        return false;
+  btnSignUp.addEventListener("click", function() {
+    var valid = document.querySelector("#loginForm").reportValidity();
+
+    if (!valid) {
+      return false;
     }
 
-//Create a user using email and password (auth).
+    //Create a user using email and password (auth).
 
     const email = txtEmail.value;
     const pass = txtPassword.value;
     const auth = firebase.auth();
-
-    const promise = auth.createUserWithEmailAndPassword(email,pass);
-    promise.catch( e => console.log(e.message));
-
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
   });
-    
+
   //App appereance when user is logged in/out.
 
   firebase.auth().onAuthStateChanged(firebaseUser => {
-    if(firebaseUser){
-
-        console.log("logged in");
-        btnLogOut.classList.remove('hide');
-        btnLogIn.classList.add('hide');
-        $("section").hide( "blind", {direction: "horizontal"}, 700 );
-        $("#homepage").show( "fold", {horizFirst: true}, 700 );
-        $('#lowerNav, #upperNav').fadeIn(1000);
-        $('.active').removeClass('active');
-        $('[data-name="homepage"]').addClass('active');
-        $('#logout').show();
-        $('#loginPage').hide();
-        alert('You have succesfully Logged in,Welcome to NYSL!');
-
+    if (firebaseUser) {
+      console.log("logged in");
+      btnLogOut.classList.remove("hide");
+      btnLogIn.classList.add("hide");
+      $("section").hide("blind", { direction: "horizontal" }, 700);
+      $("#homepage").show("fold", { horizFirst: true }, 700);
+      $("#lowerNav, #upperNav").fadeIn(1000);
+      $(".active").removeClass("active");
+      $('[data-name="homepage"]').addClass("active");
+      $("#logout").show();
+      $("#loginPage").hide();
+      alert("You have succesfully Logged in,Welcome to NYSL!");
     } else {
-
-        console.log("not logged in");
-        btnLogOut.classList.add('hide');
-        btnLogIn.classList.remove('hide');
-        $('#logout').hide();
-        $('#loginPage').show();
+      console.log("not logged in");
+      btnLogOut.classList.add("hide");
+      btnLogIn.classList.remove("hide");
+      $("#logout").hide();
+      $("#loginPage").show();
     }
-
   });
 
-  //Create an event listener for the post button(chat). 
+  //Create an event listener for the post button(chat).
 
   postButton.addEventListener("click", function() {
-    var chatUser = usernameInput.value;
     var chatText = textInput.value;
     var db = firebase.database();
-    db.ref('post').push({username:chatUser, text:chatText});
+    db.ref("post")  .push({ username: firebase.auth().currentUser.email, text: chatText });
     textInput.value = "";
-
   });
 
-    /** Function to add a data listener to chat **/
-    var startListening = function() {
-  
-      db.ref('post').on('child_added', function(snapshot) {
-        var msg = snapshot.val();
-      
-        var msgUsernameElement = document.createElement("b");
-        msgUsernameElement.textContent = msg.username;
-        
-        var msgTextElement = document.createElement("p");
-        msgTextElement.textContent = msg.text;
-  
-        var msgElement = document.createElement("div");
-        msgElement.appendChild(msgUsernameElement);
-        msgElement.appendChild(msgTextElement);
-        msgElement.className = "msg";
-        document.getElementById("results").appendChild(msgElement);
-      });
-    }
+  /** Function to add a data listener to chat **/
+  var startListening = function() {
+    db.ref("post").on("child_added", function(snapshot) {
+      var msg = snapshot.val();
 
-    // Begin listening to data from firebase chat.
-    startListening();
+      var msgUsernameElement = document.createElement("b");
+      msgUsernameElement.textContent = msg.username;
 
-}());
+      var msgTextElement = document.createElement("p");
+      msgTextElement.textContent = msg.text;
+
+      var msgElement = document.createElement("div");
+      msgElement.appendChild(msgUsernameElement);
+      msgElement.appendChild(msgTextElement);
+      msgElement.className = "msg";
+      document.getElementById("results").appendChild(msgElement);
+    });
+  };
+
+  // Begin listening to data from firebase chat.
+  startListening();
+})();
--- public/js/script.js
+++ public/js/script.js
@@ -1,85 +1,85 @@
-function DataHandler(){ //Create the single source of truth.
-    
-    var that = this;
-    var teams;
+function DataHandler() {
+  //Create the single source of truth.
 
-    // Navigation
+  var that = this;
+  var teams;
 
-    $('#lowerNav ul li, #fifaRulesNav, #logout, #loginPage, #continue').on('click', function(){
-        $("section").hide( "blind", {direction: "horizontal"}, 700 );
-        var name = $(this).data('name');
-        $("#"+name).show( "fold", {horizFirst: true}, 700 );
-        $('#upperNav').removeClass('open');
-        $('.active').removeClass('active');
-        $(this).addClass('active');
-        
-    });
-    
-    $('#continue').on('click', function(){
-        $('#lowerNav, #upperNav').fadeIn(1000);
-        $('.active').removeClass('active');
-        $('[data-name="homepage"]').addClass('active');
-        
-    });
+  // Navigation
 
-    //Upper navbar open and close toggler.
+  
+  $("#lowerNav ul li, #fifaRulesNav, #logout, #loginPage, #continue"
+  ).on("click", function() {
+    $("section").hide("blind", { direction: "horizontal" }, 700);
+    var name = $(this).data("name");
+    $("#" + name).show("fold", { horizFirst: true }, 700);
+    $("#upperNav").removeClass("open");
+    $(".active").removeClass("active");
+    $(this).addClass("active");
+  });
 
-    $('.navbar-toggler').on('click', function(event) {
-        event.preventDefault();
-        $(this).closest('.navbar-minimal').toggleClass('open');
-            
-    });
+  $("#continue").on("click", function() {
+    $("#lowerNav, #upperNav").fadeIn(1000);
+    $(".active").removeClass("active");
+    $('[data-name="homepage"]').addClass("active");
+  });
 
-    //Removal of lower and upper navbar when leaving to login page.
+  //Upper navbar open and close toggler.
 
-    $('#loginPage').on('click', function() {
-    $('#lowerNav, #upperNav').hide();
+  $(".navbar-toggler").on("click", function(event) {
+    event.preventDefault();
+    $(this).closest(".navbar-minimal").toggleClass("open");
+  });
 
-    });
-        
-    //Get the JSON file with the league standings data.
+  //Removal of lower and upper navbar when leaving to login page.
 
-    $.getJSON("data/teams_info.json", function(response){
-            that.teams = response;
-            that.createTable();
-            that.chatNav();   
-        })
-    
-//Create the standings table from the JSON file using a each loop.    
-    
-    this.createTable = function() {
+  $("#loginPage").on("click", function() {
+    $("#lowerNav, #upperNav").hide();
+  });
+
+  //Get the JSON file with the league standings data.
+
+  $.getJSON("data/teams_info.json", function(response) {
+    that.teams = response;
+    that.createTable();
+  });
+
+  //Create the standings table from the JSON file using a each loop.
+
+  this.createTable = function() {
     var rowArray = [];
     $.each(this.teams.teams, function(i, e) {
-        var row = $('<tr>');
-        row.append($('<td>').text(e.position));
-        row.append($('<td>').text(e.name));
-        row.append($('<td>').text(e.goals_scored));
-        row.append($('<td>').text(e.wins));
-        row.append($('<td>').text(e.loses));
-        row.append($('<td>').text(e.ties));
-        row.append($('<td>').text(e.points));
-        rowArray.push(row);
+      var row = $("<tr>");
+      row.append($("<td>").text(e.position));
+      row.append($("<td>").text(e.name));
+      row.append($("<td>").text(e.goals_scored));
+      row.append($("<td>").text(e.wins));
+      row.append($("<td>").text(e.loses));
+      row.append($("<td>").text(e.ties));
+      row.append($("<td>").text(e.points));
+      rowArray.push(row);
     });
-    $('#leagueTable').append(rowArray);
-    };
+    $("#leagueTable").append(rowArray);
+  };
 
+  //Limit access to chat for registerd users only.
+  this.chatNav = function() {
+    $("#messageBoard").on("click", function() {
+      if (firebase.auth().currentUser) {
+        $("section").hide();
+        $("#message").show();
+        $(".open").removeClass("open");
+        $(".active").removeClass("active");
+      } else {
+        alert("This feature is for registerd users only");
+        $(".open").removeClass("open");
+        $(".active").addClass("active");
+      }
+    });
+  };
 
-    this.chatNav = function() {
-        $('#messageBoard').on('click', function() {            
-            if (firebase.auth().currentUser) {
-                $('section').hide();
-                $('#message').show();
-                $('.open').removeClass('open');
-            } else {
-                alert('This feature is for registerd users only');
-                $('.open').removeClass('open');
+  //Call the function that limit access to chat for registerd users only
+  that.chatNav();
+}
 
-            }
-
-        });
-    };
-};
-
-   
 //initialize DataHandler
 var dataHandler = new DataHandler();
